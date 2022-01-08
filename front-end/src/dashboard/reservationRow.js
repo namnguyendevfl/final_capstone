3d5f6649215414d546d68ebbc1a14525223b2a2b
namnguyendevfl <namnguyenfl990@gmail.com>
2022-01-08 12:47:13 -0500

    added cancel and edit btns to reservationRow

--- front-end/src/dashboard/reservationRow.js
+++ front-end/src/dashboard/reservationRow.js
@@ -2,6 +2,14 @@ import React from "react"
 
 export default function ReservationRow({reservation}) {
     if (!reservation || reservation.status === "finished") return null;
+    
+    const handleCancel = () => {
+        if(window.confirm("Do you want to cancel this reservation? This cannot be undone.")) {
+            // api call will go here eventually
+    
+            window.location.reload(); 
+        }
+    }
     return (
         <>
             <tr>
@@ -12,6 +20,19 @@ export default function ReservationRow({reservation}) {
                     <td>{reservation.reservation_time}</td>
                     <td>{reservation.people}</td>
                     <td>{reservation.status}</td>
+
+                    <td>
+                        <a href={`/reservations/${reservation.reservation_id}/edit`}>
+                            <button type="button">Edit</button>
+                        </a>
+                    </td>
+
+                    <td>
+                        { /* the cancel button requires a data-reservation-id-cancel attribute for the tests */ }
+                        <button type="button" onClick={handleCancel} data-reservation-id-cancel={reservation.reservation_id}>
+                            Cancel
+                        </button>
+                    </td>
                     {reservation.status === "booked" &&
                         <td>
                             <a href={`/reservations/${reservation.reservation_id}/seat`}>
