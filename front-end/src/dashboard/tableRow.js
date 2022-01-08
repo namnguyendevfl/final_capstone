d14db54eb4bb0fef72bbb737fd4d22dc8d04081c
namnguyendevfl <namnguyenfl990@gmail.com>
2022-01-08 10:15:49 -0500

    done with US-5-finish-an-occupied-table-front

--- front-end/src/dashboard/tableRow.js
+++ front-end/src/dashboard/tableRow.js
@@ -1,14 +1,28 @@
 import React from "react";
+import { useHistory } from "react-router";
 
 export default function TableRow({ table }) {
+	const history = useHistory();
+
 	if(!table) return null;
 
+	function handleFinish() {
+		if(window.confirm("Is this table ready to seat new guests? This cannot be undone.")) {
+			// delete request here, we will add this later
+			history.push("/dashboard");
+		}
+	}
 	return (
 		<tr>
 			<th scope="row">{table.table_id}</th>
 			<td>{table.table_name}</td>
 			<td>{table.capacity}</td>
 			<td data-table-id-status={table.table_id}>{table.status}</td>
+			{table.status === "occupied" && 
+				<td data-table-id-finish={table.table_id}>
+					<button onClick = {handleFinish}>Finish</button>
+				</td>
+			}
 		</tr>
 	);
 }
